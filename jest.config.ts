250a02b0ad7dd2a8cd1cb7845b18baa9d6129aac
Radu Irbe <radu.irbe@gmail.com>
2025-01-09 08:12:31 +0200

    Initial project setup with TypeScript and testing dependencies"

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+}
